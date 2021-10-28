2e66acc39f7078b1db14494abe532c351b975d2e
epersson1 <nellepersson@gmail.com>
2021-10-27 17:53:02 -0700

    Added three potential versions of intensity calculation

--- xrd-to-pdf.py
+++ xrd-to-pdf.py
@@ -12,23 +12,58 @@ import math
 from gen_xrd import get_pattern
 
 
-# THE DATA
-struc = mg.core.Structure.from_file('MnO.cif')
+# THE DATA - SIMULATED
+# struc = mg.core.Structure.from_file('MnO.cif')
+#
+# min_angle, max_angle = 10.0, 80.0
+# wavelength = 1.5406
+# calculator = xrd.XRDCalculator(wavelength=wavelength)
+# pattern = calculator.get_pattern(struc, two_theta_range=(min_angle, max_angle), scaled=False)
+# two_angles, intensities = pattern.x, pattern.y
 
-min_angle, max_angle = 10.0, 80.0
-wavelength = 1.5406
-calculator = xrd.XRDCalculator(wavelength=wavelength)
-pattern = calculator.get_pattern(struc, two_theta_range=(min_angle, max_angle), scaled=False)
-two_angles, intensities = pattern.x, pattern.y
+
+
+# THE DATA - EXPERIMENTAL
+structure = 'MnO'
+path = 'Experimental/Clean/'
+two_angles, intensities = [], []
+with open(path + structure + '.xy', 'r') as f:
+    contents = f.readlines()
+    for line in contents:
+        if 'Wavelength' in line:
+            wavelength = float(line.split("=")[1].split(" ")[1])
+        else:
+            vals = line.split("\t")
+            if len(vals) == 2:
+                two_angles.append(float(vals[0]))
+                intensities.append(float(vals[1].strip('\n')))
+            else:
+                print(vals)
+two_angles = np.array(two_angles)
+intensities = np.array(intensities)
+
+
+# Processing
 Q_vals = (4 * math.pi * np.sin(np.deg2rad(two_angles/2))) / wavelength
 Q_min, Q_max = min(Q_vals), max(Q_vals)
 
-#display(pd.DataFrame({"2Theta": two_angles, "Q": Q_vals, "Intensity": intensities}))
 
-def I(Q):
-    """Given Q, returns the approximate intensity obtained from data."""
-    intensity = intensities[np.argmin(np.abs(Q - Q_vals))]
-    return intensity
+def I1(Q, tol = 0.075):
+    """If there is a value in the dataset close to Q, return its intensity. Else, 0"""
+    distance = min(np.abs(Q - Q_vals))
+    if distance < tol:
+        return intensities[np.argmin(np.abs(Q - Q_vals))]
+    return 0
+
+def I2(Q):
+    """Return the intensity of the value in the dataset closest to Q"""
+    return intensities[np.argmin(np.abs(Q - Q_vals))]
+
+def I3(Q):
+    """Return value if Q has been observed, else 0"""
+    if Q in Q_vals:
+        return intensities[np.argmin(np.abs(Q - Q_vals))]
+    return 0
 
 # THE THINGS YOU CALCULATE FROM THE MATERIAL
 def scattering_factors():
@@ -45,8 +80,8 @@ def scattering_factors():
         c = row['c'].iloc[0]
         factors.append((a,b,c))
     return dict(zip(ions, factors))
-factors = scattering_factors()
 
+factors = scattering_factors()
 def b(q):
     """Returns the average scattering amplitude of the material"""
     # NOTE: this is confirmed correct via the webpage
@@ -58,10 +93,9 @@ def b(q):
     return np.mean(vals)
 
 
-
 # THE MATH
 def S(Q):
-    return I(Q) / (b(Q)**2)
+    return I3(Q) / (b(Q)**2)
 
 def F(Q):
     return Q * (S(Q) - 1)
@@ -87,5 +121,4 @@ def plot_pdf(r_min, r_max):
     plt.tight_layout()
     plt.show()
 
-#plot_pdf(0, 10)
-print(S(100))
+plot_pdf(0, 10)
